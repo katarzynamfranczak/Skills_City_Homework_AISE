fe73ca301a76b20f17e515a41acf0b59e49f3afc
Katarzyna Franczak <k.franczak123@gmail.com>
2024-07-02 23:39:51 +0100

    added input user

--- javascript_form_validation/loop.js
+++ javascript_form_validation/loop.js
@@ -1,5 +1,10 @@
-
-
+let userInput = prompt ("What's your name?");
+if (userInput.trim() == null || userInput.trim() == ""){ //function .trim to remove spaces for the start and the end of the string
+    userInput = "Mystery Person"
+        alert(userInput + ", finally you've ve arrived! Just on time to see what this web can do.");
+}else {
+    alert("Hello, " + userInput + "! Let's find out what this web does!");
+    }
 var number = [-5, -2,-6,0,10];
 var largest = number[0];
 alert("There is a list made out of " + number.length + " numbers.");
@@ -12,3 +17,4 @@ for (var a =0; a<number.length; a++){
 }     
 console.log("The largest number is: " + largest);
 alert("The largest number is " + largest + " ! YAY!");
+alert("Dear " + userInput + ", Farewell! I hope you've had fun with loops in js!")
