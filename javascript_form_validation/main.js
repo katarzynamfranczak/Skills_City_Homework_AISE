b0f3cc302325a30a0671f6e76c6040b55e5d1f5f
Katarzyna Franczak <k.franczak123@gmail.com>
2024-07-02 08:58:09 +0100

    add validation for the rest of the homework

--- javascript_form_validation/main.js
+++ javascript_form_validation/main.js
@@ -12,4 +12,55 @@ function validateInputs() {
         nameInvalidMessageElement.style.display = "";
         nameInvalidMessageElement.style.color = "red";
     }
+   
+    var isEmailInputValid = true;
+    var emailInput = document.getElementsByName('email')[0];
+    var emailInputValue = emailInput.value;
+    if (emailInputValue == ""){
+        isEmailInputValid = false;
+    }
+    var emailInvalidMessageElement = document.getElementsByName('emailInvalidMessage')[0];
+    if (!isEmailInputValid){
+        emailInvalidMessageElement.style.display="";
+        emailInvalidMessageElement.style.color ="red";
+    }
+
+    var isContactNumberValid = true;
+    var contactNumberInput = document.getElementsByName('contactNumber')[0];
+    var contactNumberValue = contactNumberInput.value;
+    if (contactNumberValue ==""){
+        isContactNumberValid = false;
+    }
+    var contactNumberInvalidMessageElement = document.getElementsByName('contactNumberInvalidMessage')[0];
+    if (!isContactNumberValid){
+        contactNumberInvalidMessageElement.style.display="";
+        contactNumberInvalidMessageElement.style.color ="red";
+    }
+
+    var isCityValid = true;
+    var cityInput = document.getElementsByName('city')[0];
+    var cityValue = cityInput.value;
+    if (cityValue ==""){
+        isCityValid = false;
+    }
+
+    var cityInvalidMessageElement = document.getElementsByName('cityInvalidMessage')[0];
+    if (!isCityValid){
+        cityInvalidMessageElement.style.display="";
+        cityInvalidMessageElement.style.color ="red";
+    }
+
+    var isDescriptionValid = true;
+    var descriptionInput = document.getElementsByName('city')[0]; //scans the html, it searches for tags that have the name called city
+    var descriptionValue = descriptionInput.value;
+    if (descriptionValue ==""){
+        isDescriptionValid = false;
+    }
+
+    var descriptionInvalidMessageElement = document.getElementsByName('descriptionInvalidMessage')[0];
+    if (!isDescriptionValid){
+        descriptionInvalidMessageElement.style.display="";
+        descriptionInvalidMessageElement.style.color ="red";
+    }
+
 }
